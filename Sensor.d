affcee00fc281723dff3bdc0c341fb3540a614c3
tarikjaber <55803936+tarikjaber@users.noreply.github.com>
2022-04-04 12:32:18 -0400

    Remove .d, .map, and .s19 files

--- Sensor.d
+++
@@ -1,7 +0,0 @@
-../Sensor.d: ../Sensor.cpp ../Sensor.h Libraries/FEHIO.h \
- Startup/derivative.h Startup/MK60DZ10.h Drivers/adc16.h
-../Sensor.h:
-Libraries/FEHIO.h:
-Startup/derivative.h:
-Startup/MK60DZ10.h:
-Drivers/adc16.h:
--- Servo.d
+++
@@ -1,3 +0,0 @@
-../Servo.d: ../Servo.cpp ../Servo.h Libraries/FEHServo.h
-../Servo.h:
-Libraries/FEHServo.h:
